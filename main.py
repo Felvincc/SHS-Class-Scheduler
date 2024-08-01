80a6d5659a82113dfcb2f13d0835021e3985f0eb
Felvincc <felvincedy1@gmail.com>
2024-08-02 00:15:32 +0800

    info gathering
    trying to implement the information-gathering part.
    
    working on storing the info of the number of buildings, and how many floors are in each one, then eventually how many rooms per floor.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-
+import sys
 
 class create:
 
@@ -33,11 +33,12 @@ class declare:
         
         return "building_"+str(a)
 
-class system:
+class system: # i am unsure if system is the best name for this class...
         
     def info_gathering():
         num_buildings_list = list()
         a=0
+        b=0
 
         #num_levels = int(input("How many grade levels is present?: "))
 
@@ -52,7 +53,7 @@ class system:
         #num_teachers = int(input("How many teachers will be teaching?: "))
 
         
-        for x in range(num_buildings):
+        for x in range(num_buildings): # Makes a dictionary for the buildings (building_1, building_2...) these values will be assigned a value based on how many floors the building has.
             a+=1
             building_name_temp = declare.building_floors(a)
             dict_buildings=dict()
@@ -60,7 +61,15 @@ class system:
 
             print(dict_buildings)
 
+        building_name_temp=None
 
+        for x in range(num_buildings): # take input for names for the buildings, and matches it with the blah blah blah you get the point
+            b+=1
+            building_name_temp = declare.building_floors(b)
+            building_name_input_temp = str(input("\n\nRename Building_"+str(b)+": "))
+            dict_buildings[building_name_temp]=building_name_input_temp
+
+        print(dict_buildings)
 
             
 
@@ -70,6 +79,7 @@ class system:
 
 
 
+        exit()
 
         num_building_floors = int(input("How many floors are there in building(s)"))
 
