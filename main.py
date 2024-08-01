15df37581fc78c5a0f94515d61416c694da5baf0
Felvincc <felvincedy1@gmail.com>
2024-08-01 23:36:54 +0800

    start

---
+++ main.py
@@ -0,0 +1,88 @@
+
+
+
+class create:
+
+    def section(x):
+
+        pass
+
+    def classroom():
+
+        pass
+
+    def teacher():
+
+        pass
+
+    def subject():
+
+        pass
+
+    def building():
+
+        pass
+
+    def level():
+
+        pass
+
+class declare:
+    
+    def building_floors(a):
+        
+        return "building_"+str(a)
+
+class system:
+        
+    def info_gathering():
+        num_buildings_list = list()
+        a=0
+
+        #num_levels = int(input("How many grade levels is present?: "))
+
+        num_buildings = int(input("How many buildings are there?: "))
+
+        #num_classrooms = int(input("How many clasrooms are there?: "))
+
+        #num_sections = int(input("How many sections will there be in a grade level?: "))
+
+        #num_subjects = int(input("How many subjects are there?: "))
+
+        #num_teachers = int(input("How many teachers will be teaching?: "))
+
+        
+        for x in range(num_buildings):
+            a+=1
+            building_name_temp = declare.building_floors(a)
+            dict_buildings=dict()
+            dict_buildings[building_name_temp] = "num_floors_temp"
+
+            print(dict_buildings)
+
+
+
+            
+
+            
+
+
+
+
+
+
+        num_building_floors = int(input("How many floors are there in building(s)"))
+
+
+
+class ui():
+
+    def start():
+        system.info_gathering()
+
+
+ui.start()
+
+
+        
+
